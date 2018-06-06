ef43234dd822ff88ece4b616c3ac938b63cb9234
eveyrdayx <everydayx@163.com>
2018-06-06 13:15:25 +0800

    20180606

---
+++ client.py
@@ -0,0 +1,17 @@
+import socket
+
+
+def client():
+    HOST = '127.0.0.1'
+    PORT = 8888
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.connect((HOST,PORT))
+    while True:
+        msg = bytes(input(">>: "),encoding='utf8')
+        s.sendall(msg)
+        data = s.recv(1024)
+        print('Recivied',repr(data))
+    s.close()
+
+if __name__ == '__main__':
+    client()
---
+++ server.py
@@ -0,0 +1,27 @@
+import sys,socket,time,gevent
+from gevent import monkey
+
+monkey.patch_all()
+
+def server(port):
+    s = socket.socket()
+    s.bind(('0.0.0.0',port))
+    s.listen(500)
+    while True:
+        cli,addr = s.accept()
+        gevent.spawn(handle_request,cli)  #一生成请求 就交给一个协程来处理
+def handle_request(conn):
+    try:
+        while True:
+            data = conn.recv(1024)
+            print('Recv:',data)
+            conn.send(data)
+            if not data:
+                conn.shutdown(socket.SHUT_WR)
+    except Exception as E:
+        print(E)
+    finally:
+        conn.close()
+
+if __name__ == '__main__':
+    server(8888)
