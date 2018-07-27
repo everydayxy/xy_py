f29e6c45f657ce06c736e45c7faed65f3a52a74e
eveyrdayx <everydayx@163.com>
2018-07-27 16:31:47 +0800

    20180727

--- test.py
+++ test.py
@@ -1,24 +1,2 @@
-import threading
-
-local = threading.local()
-class Student():
-    def __init__(self, name, age, score):
-        self.name = name
-        self.age = age
-        self.score = score
-
-def getInfo():
-    stu = local.student
-    print('this is %s, %d, %d in %s' %(stu.name, stu.age, stu.score, threading.current_thread().name))
-
-def setInfo(name, age, score):
-    local.student = Student(name, age, score)
-    getInfo()
-
-t1 = threading.Thread(target=setInfo, args=('Alice', 25, 88), name='Thread_1')
-t2 = threading.Thread(target=setInfo, args=('Bob', 27, 90), name='Thread_2')
-t1.start()
-t2.start()
-t1.join()
-t2.join()
-print('print complete...')
+for j, k in enumerate(range(5)):
+    print(j,k+1)
