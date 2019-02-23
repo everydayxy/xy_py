9bfd96aa81a74e00d862954152c208eff967d9ef
everydayxy <everydayx@163.com>
2019-02-23 19:48:03 +0800

    20190223 登录逻辑完成

---
+++ magcore_xy/magcore_monitor_api.md
@@ -0,0 +1,3 @@
+### magcore monritor 客户端显示接口
+#### 浏览器打开 http://magcore.clawit.com/pages/monitor.html?svr=test&gid=your_Game_id
+#### your_Game_id这里调用你自己创建的gameid即可
--- magcore_xy/main_function.py
+++ magcore_xy/main_function.py
@@ -32,21 +32,23 @@ def join_game(playerid):
         '3': '已销毁'
     }
     GetGameLists_ret = GetGameList()
-    while True:
-        print('请在下列的游戏列表选择一个你要加入的游戏')
-        zipped_id = zip(['{}'.format(x) for x in range(1, len(GetGameLists_ret) + 1)], GetGameLists_ret)
-        zipped_id_dict = {}
-        for k, v in zipped_id:
-            print('游戏房间编号: {} , 游戏id: {},地图尺寸: {}, 游戏状态: {}'.format(k,v['id'],map_description_dict[v['map']],game_status_description_dict[str(v['state'])]))
-            zipped_id_dict[k] = v['id']
-        your_choice_gameid_num = input('请输入你需要选择的游戏编号(只能选择游戏状态为等待加入的): ')
+    print('请在下列的游戏列表选择一个你要加入的游戏')
+    zipped_id = zip(['{}'.format(x) for x in range(1, len(GetGameLists_ret) + 1)], GetGameLists_ret)
+    zipped_id_dict = {}
+    for k, v in zipped_id:
+        print('游戏房间编号: {} , 游戏id: {},地图尺寸: {}, 游戏状态: {}'.format(k,v['id'],map_description_dict[v['map']],game_status_description_dict[str(v['state'])]))
+        zipped_id_dict[k] = v['id']
+    your_choice_gameid_num = input('请输入你需要选择的游戏编号(只能选择游戏状态为等待加入的): ')
+    try:
         your_choice_gameid = zipped_id_dict[your_choice_gameid_num]
-        JoinGame_flag = JoinGame(your_choice_gameid, playerid)
-        if JoinGame_flag == True:
-            print('加入游戏成功')
-            break
-        else:
-            print('请重新选择游戏')
+    except KeyError:
+        print('没有这个游戏编号')
+    JoinGame_flag = JoinGame(your_choice_gameid, playerid)
+    if JoinGame_flag == True:
+        print('加入游戏成功')
+        return your_choice_gameid
+    else:
+        return None
 
 def create_game():
     while True:
@@ -57,50 +59,73 @@ def create_game():
         except KeyError:
             print('请输入正确的地图编号！')
     gameid = CreateGame(map_name)
-    GetMap_ret = GetMap(map_name)
-    return gameid,GetMap_ret
+    return gameid
 
 
 def whether_join_exiting_game(playerid):
     join_or_not = int(input('1:是\n0:否\n是否加入现有游戏: '))
     # 加入现有游戏
     if join_or_not == 1:
-        create_or_not = int(input('1:是\n0:否\n是否创建游戏: '))
-        # 不创建游戏，加入游戏
-        if create_or_not == 0:
-            join_game(playerid)
-            return 0,0
-        # 创建游戏
-        elif create_or_not == 1:
-            gameid , GetMap_ret = create_game()
-            return gameid,GetMap_ret
+        gameid = join_game(playerid)
+        return gameid
 
     # 不加入现有游戏
     elif join_or_not == 0:
-        gameid , GetMap_ret = create_game()
-        return gameid, GetMap_ret
+        gameid = create_game()
+        return gameid
 
 def waiting_for_play_game(gameid):
+    count = 0
     while True:
         StartGame_flag = StartGame(gameid)
         if StartGame_flag == True:
             return True
         else:
+            if count > 5:
+                print('重试次数过多，请重新进入！')
+                return False
             print('等待玩家进入。。。。')
             time.sleep(5)
+            count += 1
+
+def find_base_location(gameid,playerindex):
+    enemy_base_location = []
+    other_player_index = []
+    GetGame_ret_dict = GetGame(gameid)
+    for y in GetGame_ret_dict['Players']:
+            if y['Index'] != playerindex and y['Index'] != 0 :
+                other_player_index.append(y['Index'])
+
+    for x in GetGame_ret_dict['Cells']:
+        for i in x:
+            if i['Type'] == 2 and i['Owner'] in other_player_index:
+                print(i['X'],i['Y'])
+                enemy_base_location.append([i['X'],i['Y']])
+
+    return enemy_base_location
 
 def main():
     # 获取玩家初始化状态
     player_init_status_json = create_player()
     # 获取玩家playerid
-    playerid = player_init_status_json['Id']
-    # 是否加入已有游戏
-    gameid, GetMap_ret = whether_join_exiting_game(playerid)
-    if gameid != 0 and GetMap_ret != 0:
-        # 等待玩家进入
-        flag = waiting_for_play_game(gameid)
-        if flag:
-            print('玩家进入成功')
+    myplayerid = player_init_status_json['Id']
+    # 获取玩家index
+    myplayerindex = player_init_status_json['Index']
+    while True:
+        # 是否加入已有游戏
+        gameid = whether_join_exiting_game(myplayerid)
+        if gameid:
+            # 等待玩家进入
+            flag = waiting_for_play_game(gameid)
+            if flag:
+                print('玩家进入成功')
+                print('目前的gameid: {}'.format(gameid))
+                break
+        else:
+            pass
+    enemy_base_location = find_base_location(gameid,myplayerindex)
+    print(enemy_base_location)
+
 
 if __name__ == '__main__':
     main()
